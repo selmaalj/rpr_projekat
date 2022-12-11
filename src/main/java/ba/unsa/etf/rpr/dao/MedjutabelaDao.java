5b6a6de1be7f4a8b39790cfbaa3739a435867e40
selmaalj <sljuhar1@etf.unsa.ba>
2022-12-11 21:14:55 +0100

    MedjutabelaDao

---
+++ src/main/java/ba/unsa/etf/rpr/dao/MedjutabelaDao.java
@@ -0,0 +1,30 @@
+package ba.unsa.etf.rpr.dao;
+
+import java.util.List;
+
+public class MedjutabelaDao implements Dao{
+    @Override
+    public Object getbyId(int id) {
+        return null;
+    }
+
+    @Override
+    public Object add(Object element) {
+        return null;
+    }
+
+    @Override
+    public Object update(Object element) {
+        return null;
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+
+    @Override
+    public List getAll() {
+        return null;
+    }
+}
