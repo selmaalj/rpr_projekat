018ffbe0bdafea1775ee7b93bbf53d3a38adb07e
selmaalj <sljuhar1@etf.unsa.ba>
2022-12-11 21:16:02 +0100

    PredmetDao

---
+++ src/main/java/ba/unsa/etf/rpr/dao/PredmetDao.java
@@ -0,0 +1,31 @@
+package ba.unsa.etf.rpr.dao;
+
+import java.sql.*;
+import java.util.List;
+
+public class PredmetDao implements Dao{
+    @Override
+    public Object getbyId(int id) {
+        return null;
+    }
+
+    @Override
+    public Object add(Object element) {
+        return null;
+    }
+
+    @Override
+    public Object update(Object element) {
+        return null;
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+
+    @Override
+    public List getAll() {
+        return null;
+    }
+}
