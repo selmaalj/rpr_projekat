fa64c39407e687a23fb825326b8a0a4070e2b1cc
selmaalj <sljuhar1@etf.unsa.ba>
2022-12-11 21:12:44 +0100

    Dao interface

---
+++ src/main/java/ba/unsa/etf/rpr/dao/Dao.java
@@ -0,0 +1,11 @@
+package ba.unsa.etf.rpr.dao;
+
+import java.util.List;
+
+public abstract interface  Dao<Tip> {
+    Tip getbyId(int id);
+    Tip add(Tip element);
+    Tip update(Tip element);
+    void delete(int id);
+    List<Tip> getAll();
+}
