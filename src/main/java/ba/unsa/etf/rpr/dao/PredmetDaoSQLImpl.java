1726967c9f1265abe7316eb58e33b00672e47a0a
selmaalj <sljuhar1@etf.unsa.ba>
2022-12-13 23:12:57 +0100

    male promjene

--- src/main/java/ba/unsa/etf/rpr/dao/PredmetDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/PredmetDaoSQLImpl.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Scanner;
 
 
-public class PredmetDaoSQLImpl implements Dao<Predmet>{
+public class PredmetDaoSQLImpl implements PredmetDao{
     private Connection con;
     public PredmetDaoSQLImpl() {
         try{
