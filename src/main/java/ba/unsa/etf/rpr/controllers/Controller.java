83eb683e6de7918c8d2ee89028167569609071f4
selmaalj <sljuhar1@etf.unsa.ba>
2022-12-18 16:39:46 +0100

    dodana metoda getIds

--- src/main/java/ba/unsa/etf/rpr/controllers/Controller.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/Controller.java
@@ -1,15 +1,26 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.dao.PredmetDao;
+import ba.unsa.etf.rpr.dao.PredmetDaoSQLImpl;
+import ba.unsa.etf.rpr.tabele.Instruktor;
+import ba.unsa.etf.rpr.tabele.Predmet;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.util.List;
 
 
 public class Controller {
@@ -19,6 +30,9 @@ public class Controller {
     public Text text;
     public TextField textfield;
     public Separator separator;
+    private Predmet p;
+    private PredmetDaoSQLImpl pd;
+    private Instruktor ins;
     @FXML
     void initialize() {
         choiceBox.getItems().addAll("Osnovna", "Srednja", "Fakultet");
@@ -27,10 +41,31 @@ public class Controller {
         slider.setBlockIncrement(10);
         slider.setShowTickLabels(true);
         slider.setShowTickMarks(true);
+        p=new Predmet();
+        pd=new PredmetDaoSQLImpl();
+        ins=new Instruktor();
     }
-    public void akcijaDugmeta(ActionEvent actionEvent) {
+    public double getCijenapoCasu(){
+        return ins.getCijenaPoCasu();
+    }
+    public List<Integer> getIds(){
+        return pd.getIds(p);
+    }
+    public void akcijaDugmeta(ActionEvent actionEvent) throws IOException {
+        p.setNivoSkolovanja(choiceBox.getValue());
+        p.setNazivPredmeta(textfield.getText());
+        ins.setCijenaPoCasu(slider.getValue());
+        System.out.println("Cijena po casu "+ins.getCijenaPoCasu());
+        System.out.println("Nivo skolovanja: "+p.getNivoSkolovanja()+" naziv predmeta "+p.getNazivPredmeta());
         Node node= (Node) actionEvent.getSource();
         Stage stage= (Stage) node.getScene().getWindow();
         stage.close();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/secondwindow.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root, 600, 400);
+        stage.setTitle("Vaši instruktori!");
+        //stage.getIcons().add(new Image("file:///C:/Users/WIN10/Downloads/teacher.png"));
+        stage.setScene(scene);
+        stage.show();
     }
 }
--- src/main/java/ba/unsa/etf/rpr/dao/PredmetDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/PredmetDao.java
@@ -2,5 +2,8 @@ package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.tabele.Predmet;
 
+import java.util.List;
+
 public interface PredmetDao extends Dao<Predmet>{
+    public List<Integer> getIds(Predmet p);
 }
