fbeded7a0c3126396bc4b7ee9990575305cf5a2c
selmaalj <sljuhar1@etf.unsa.ba>
2022-12-19 21:31:08 +0100

    App

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,13 +1,34 @@
 package ba.unsa.etf.rpr;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App extends Application
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/fxmlfile.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root, 600, 400);
+        stage.setMinHeight(325);
+        stage.setMinWidth(475);
+        stage.setMaxHeight(500);
+        stage.setMaxWidth(700);
+        stage.setTitle("Dobro došli!");
+        stage.getIcons().add(new Image("file:///C:/Users/WIN10/Downloads/book.png"));
+        stage.setScene(scene);
+        stage.show();
+    }
+    public static void main (String[] args) {
+        launch();
     }
 }
