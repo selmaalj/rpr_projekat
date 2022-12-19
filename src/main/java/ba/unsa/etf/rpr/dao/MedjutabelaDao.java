35bf3624899ebd819c6e2875c4cef3079a298042
selmaalj <sljuhar1@etf.unsa.ba>
2022-12-19 20:00:21 +0100

    metoda getbyPredmet

--- src/main/java/ba/unsa/etf/rpr/dao/MedjutabelaDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/MedjutabelaDao.java
@@ -1,6 +1,10 @@
 package ba.unsa.etf.rpr.dao;
 
+import ba.unsa.etf.rpr.tabele.Instruktor;
 import ba.unsa.etf.rpr.tabele.Medjutabela;
 
+import java.util.List;
+
 public interface MedjutabelaDao extends Dao<Medjutabela> {
+    public List<Integer> getbyPredmet(int id);
 }
