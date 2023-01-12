0e0214c068cbdb53027da481d9b4431b445e5ade
selmaalj <sljuhar1@etf.unsa.ba>
2023-01-12 21:18:22 +0100

    instruktor controller klasa

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/Instruktor.java
@@ -0,0 +1,19 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class Instruktor {
+    public TextField ImeID;
+    public TextField PrezimeID;
+    public TextField EmailID;
+
+    @FXML
+    void initialize() {
+
+    }
+
+    public void prijaviSeAction(ActionEvent actionEvent) {
+    }
+}
