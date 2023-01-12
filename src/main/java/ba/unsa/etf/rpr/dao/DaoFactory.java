b4815b2de3699b7dd705d43fe7cca96075dbf0b5
selmaalj <sljuhar1@etf.unsa.ba>
2023-01-12 19:53:32 +0100

    DaoFactory push

---
+++ src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
@@ -0,0 +1,24 @@
+package ba.unsa.etf.rpr.dao;
+
+public class DaoFactory {
+    private static final PredmetDao predmetDao = PredmetDaoSQLImpl.getInstance();
+    private static final DostupanDao dostupanDao = DostupanDaoSQLImpl.getInstance();
+    private static final InstruktorDao instruktorDao = InstruktorDaoSQLImpl.getInstance();
+    private static final MedjutabelaDao medjutabelaDao = MedjutabelaDaoSQLImpl.getInstance();
+
+    private DaoFactory(){
+    }
+    public static PredmetDao predmetDao(){
+        return predmetDao;
+    }
+    public static DostupanDao dostupanDao(){
+        return dostupanDao;
+    }
+    public static MedjutabelaDao medjutabelaDao(){
+        return medjutabelaDao;
+    }
+    public static InstruktorDao instruktorDao(){
+        return instruktorDao;
+    }
+
+}
