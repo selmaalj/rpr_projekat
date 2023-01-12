a1913cce396625ed49274c63a228e8528de558c5
selmaalj <sljuhar1@etf.unsa.ba>
2023-01-12 19:55:26 +0100

    public suvišno za interface

--- src/main/java/ba/unsa/etf/rpr/dao/PredmetDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/PredmetDao.java
@@ -2,8 +2,7 @@ package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.tabele.Predmet;
 
-import java.util.List;
 
 public interface PredmetDao extends Dao<Predmet>{
-    public int getId(String predmet, String nivo);
+    int getId(String predmet, String nivo);
 }
