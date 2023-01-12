6feda08c391831d3524da5d86d74cb7131f9f34a
selmaalj <sljuhar1@etf.unsa.ba>
2023-01-12 19:57:46 +0100

    odabir window push

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/Odabir.java
@@ -0,0 +1,36 @@
+package ba.unsa.etf.rpr.controllers;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Odabir {
+    public Button korisnik;
+    public Button instruktor;
+
+    public void akcijaDugmeta1(ActionEvent actionEvent) throws IOException {
+        Node node= (Node) actionEvent.getSource();
+        Stage stage= (Stage) node.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/korisnik.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root, 600, 400);
+        stage.setTitle("Vaši instruktori!");
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void akcijaDugmeta2(ActionEvent actionEvent) throws IOException {
+        Node node= (Node) actionEvent.getSource();
+        Stage stage= (Stage) node.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/instruktor.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root, 600, 400);
+        //stage.setTitle("Vaši instruktori!");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
