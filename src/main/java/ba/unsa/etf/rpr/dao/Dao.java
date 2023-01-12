ec7a98f376ab4fbc843ae6e73e44879b58a4786d
selmaalj <sljuhar1@etf.unsa.ba>
2023-01-12 20:12:17 +0100

    metoda getbyid

--- src/main/java/ba/unsa/etf/rpr/dao/Dao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/Dao.java
@@ -3,7 +3,7 @@ package ba.unsa.etf.rpr.dao;
 import java.util.List;
 
 public abstract interface  Dao<Tip> {
-    Tip getbyId(int id);
+    Tip getById(int id);
     Tip add(Tip element);
     Tip update(Tip element);
     void delete(int id);
