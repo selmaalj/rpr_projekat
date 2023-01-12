923148aa0c8ca6c01a4dd53e6b0679ebea3e4b55
selmaalj <sljuhar1@etf.unsa.ba>
2023-01-12 19:52:10 +0100

    imena atributa promijenjena

--- src/main/java/ba/unsa/etf/rpr/tabele/Dostupan.java
+++ src/main/java/ba/unsa/etf/rpr/tabele/Dostupan.java
@@ -33,8 +33,8 @@ public class Dostupan implements Idable{
         return ins;
     }
 
-    public void setIns(Instruktor ins) {
-        this.ins = ins;
+    public void setIns(Object ins) {
+        this.ins = (Instruktor) ins;
     }
 
     @Override
