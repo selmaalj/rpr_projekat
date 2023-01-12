266de719b723452f94fed6d651123441e016cdcf
selmaalj <sljuhar1@etf.unsa.ba>
2023-01-12 19:55:43 +0100

    public suvišno za interface

--- src/main/java/ba/unsa/etf/rpr/dao/MedjutabelaDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/MedjutabelaDao.java
@@ -1,10 +1,9 @@
 package ba.unsa.etf.rpr.dao;
 
-import ba.unsa.etf.rpr.tabele.Instruktor;
 import ba.unsa.etf.rpr.tabele.Medjutabela;
 
 import java.util.List;
 
 public interface MedjutabelaDao extends Dao<Medjutabela> {
-    public List<Integer> getbyPredmet(int id);
+    List<Integer> getbyPredmet(int id);
 }
