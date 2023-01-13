28e2541c8ed0fbe09c71f88b9a2bf898911c039a
selmaalj <sljuhar1@etf.unsa.ba>
2023-01-13 22:28:48 +0100

    getByInstruktor metoda u DostupanDao

--- src/main/java/ba/unsa/etf/rpr/dao/DostupanDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/DostupanDao.java
@@ -1,6 +1,10 @@
 package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.tabele.Dostupan;
+import ba.unsa.etf.rpr.tabele.Instruktor;
+
+import java.util.List;
 
 public interface DostupanDao extends Dao<Dostupan> {
+    List<Dostupan> getByInstruktor(Instruktor ins);
 }
