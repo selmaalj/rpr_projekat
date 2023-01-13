fa3d257a02795e11fe722d71b9c69df7d5e9c2f4
selmaalj <sljuhar1@etf.unsa.ba>
2023-01-13 22:29:41 +0100

    getByNazivTel metoda u InstruktorDao

--- src/main/java/ba/unsa/etf/rpr/dao/InstruktorDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/InstruktorDao.java
@@ -3,4 +3,5 @@ package ba.unsa.etf.rpr.dao;
 import ba.unsa.etf.rpr.tabele.Instruktor;
 
 public interface InstruktorDao extends Dao<Instruktor>{
+    Instruktor getByNazivTel(String naziv,String tel);
 }
