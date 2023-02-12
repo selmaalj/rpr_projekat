64a538da2220ac7cff275056383d7d38ff19b4b1
selmaalj <sljuhar1@etf.unsa.ba>
2023-02-12 22:51:27 +0100

    akcijaDugmetaNazad implementirano u AdminPrijava

--- src/main/java/ba/unsa/etf/rpr/controllers/AdminPrijava.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/AdminPrijava.java
@@ -1,4 +1,23 @@
 package ba.unsa.etf.rpr.controllers;
 
-public class AdminPrijava {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class AdminPrijava{
+    public void akcijaDugmetaNazad(ActionEvent actionEvent) throws IOException {
+        Node node= (Node) actionEvent.getSource();
+        Stage stage= (Stage) node.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/odabirwindow.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root, 400, 200);
+        stage.setTitle("Dobro došli!:");
+        stage.setScene(scene);
+        stage.show();
+    }
 }
