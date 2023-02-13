f75ac249fd9de04725722a1ed47e33697d8c9a85
selmaalj <sljuhar1@etf.unsa.ba>
2023-02-13 23:47:26 +0100

    dodan izuzetak na iznos cijene

--- src/main/java/ba/unsa/etf/rpr/controllers/Instruktor.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/Instruktor.java
@@ -51,43 +51,58 @@ public class Instruktor {
     }
 
     public void prijaviSeAction(ActionEvent actionEvent) throws Exception {
-        InstruktorDaoSQLImpl ins = InstruktorDaoSQLImpl.getInstance();
-        ba.unsa.etf.rpr.domain.Instruktor i = new ba.unsa.etf.rpr.domain.Instruktor();
-        i.setNazivInstruktora(ime.getText() + " " + prezime.getText());
-        i.setId(0);
-        i.setGrad(grad.getValue());
-        i.setTelefonskiBroj(telefon.getText());
-        i.setCijenaPoCasu(Double.parseDouble(cijena.getText()));
-        i = ins.add(i);
-        ObservableList<String> ob = pregledListView.getItems();
-        PredmetDaoSQLImpl pd = PredmetDaoSQLImpl.getInstance();
-        for (String s : ob) {
-            int id = Integer.parseInt(s.split("\\.")[0]);
-            Predmet p = pd.getById(id);
-            MedjutabelaDaoSQLImpl.getInstance().add(new Medjutabela(p, i));
+        try {
+            if (Double.parseDouble(cijena.getText()) < 0 || Double.parseDouble(cijena.getText()) > 50)
+                throw new Izuzetak("Potrebno je da cijene budu u opsegu od 0 KM do 50 KM.\n" +
+                        "Ponovite unos cijene.");
+            InstruktorDaoSQLImpl ins = InstruktorDaoSQLImpl.getInstance();
+            ba.unsa.etf.rpr.domain.Instruktor i = new ba.unsa.etf.rpr.domain.Instruktor();
+            i.setNazivInstruktora(ime.getText() + " " + prezime.getText());
+            i.setId(0);
+            i.setGrad(grad.getValue());
+            i.setTelefonskiBroj(telefon.getText());
+            i.setCijenaPoCasu(Double.parseDouble(cijena.getText()));
+            i = ins.add(i);
+            ObservableList<String> ob = pregledListView.getItems();
+            PredmetDaoSQLImpl pd = PredmetDaoSQLImpl.getInstance();
+            for (String s : ob) {
+                int id = Integer.parseInt(s.split("\\.")[0]);
+                Predmet p = pd.getById(id);
+                MedjutabelaDaoSQLImpl.getInstance().add(new Medjutabela(p, i));
+            }
+            List<String> dani = new ArrayList<>();
+            if (ponedjeljak.isSelected()) dani.add("Ponedjeljak");
+            if (utorak.isSelected()) dani.add("Utorak");
+            if (srijeda.isSelected()) dani.add("Srijeda");
+            if (cetvrtak.isSelected()) dani.add("Cetvrtak");
+            if (petak.isSelected()) dani.add("Petak");
+            if (subota.isSelected()) dani.add("Subota");
+            if (nedjelja.isSelected()) dani.add("Nedjelja");
+            for (String s : dani)
+                DostupanDaoSQLImpl.getInstance().add(new Dostupan(0, s, i));
+            GMailer gm = new GMailer();
+            gm.posaljiMail("Podaci o instruktoru:" + "\nNaziv: " + ime.getText() + " " + prezime.getText() + "\nEmail adresa: " + email.getText() + "\nGrad: " + grad.getValue() + "\nCijena po času: " + cijena.getText());
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle(null);
+            alert.setHeaderText(null);
+            Stage st = (Stage) alert.getDialogPane().getScene().getWindow();
+            st.getIcons().add(new Image("file:///C:/Users/WIN10/Downloads/book.png"));
+            alert.setContentText("Uspješno ste se prijavili\nOčekujte naš odgovor na email!");
+            alert.showAndWait();
+            Node node = (Node) actionEvent.getSource();
+            Stage stage = (Stage) node.getScene().getWindow();
+            stage.close();
         }
-        List<String> dani = new ArrayList<>();
-        if (ponedjeljak.isSelected()) dani.add("Ponedjeljak");
-        if (utorak.isSelected()) dani.add("Utorak");
-        if (srijeda.isSelected()) dani.add("Srijeda");
-        if (cetvrtak.isSelected()) dani.add("Cetvrtak");
-        if (petak.isSelected()) dani.add("Petak");
-        if (subota.isSelected()) dani.add("Subota");
-        if (nedjelja.isSelected()) dani.add("Nedjelja");
-        for (String s : dani)
-            DostupanDaoSQLImpl.getInstance().add(new Dostupan(0, s, i));
-        GMailer gm = new GMailer();
-        gm.posaljiMail("Podaci o instruktoru:" + "\nNaziv: " + ime.getText() + " " + prezime.getText() + "\nEmail adresa: " + email.getText() + "\nGrad: " + grad.getValue() + "\nCijena po času: " + cijena.getText());
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle(null);
-        alert.setHeaderText(null);
-        Stage st = (Stage) alert.getDialogPane().getScene().getWindow();
-        st.getIcons().add(new Image("file:///C:/Users/WIN10/Downloads/book.png"));
-        alert.setContentText("Uspješno ste se prijavili\nOčekujte naš odgovor na email!");
-        alert.showAndWait();
-        Node node = (Node) actionEvent.getSource();
-        Stage stage = (Stage) node.getScene().getWindow();
-        stage.close();
+        catch(Izuzetak e) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle(null);
+            alert.setHeaderText(null);
+            Stage st = (Stage) alert.getDialogPane().getScene().getWindow();
+            st.getIcons().add(new Image("file:///C:/Users/WIN10/Downloads/book.png"));
+            alert.setContentText(e.getMessage());
+            cijena.clear();
+            alert.showAndWait();
+        }
     }
 
     public void akcijaDugmetaNazad(ActionEvent actionEvent) throws IOException {
