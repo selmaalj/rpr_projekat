54c2c4d233f732b80f5f85b91ec3079c350bfcf0
selmaalj <sljuhar1@etf.unsa.ba>
2023-02-14 18:25:11 +0100

    napunjen listview vrijednostima

--- src/main/java/ba/unsa/etf/rpr/controllers/AdminPanel.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/AdminPanel.java
@@ -1,15 +1,35 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.dao.PredmetDaoSQLImpl;
+import ba.unsa.etf.rpr.domain.Predmet;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ListView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.List;
 
 public class AdminPanel {
+
+    public ListView<String> predmeti;
+
+    @FXML
+    void initialize(){
+        ObservableList<String> ob= FXCollections.observableArrayList();
+        PredmetDaoSQLImpl pd=PredmetDaoSQLImpl.getInstance();
+        List<Predmet> l=pd.getAll();
+        for(Predmet p: l)
+            ob.add(p.getId()+"-"+p.getNazivPredmeta()+"-"+p.getNivoSkolovanja());
+        predmeti.setItems(ob);
+        predmeti.getSelectionModel().select(0);
+    }
 
     public void akcijaDugmetaNazad(ActionEvent actionEvent) throws IOException {
         Node node=(Node) actionEvent.getSource();
