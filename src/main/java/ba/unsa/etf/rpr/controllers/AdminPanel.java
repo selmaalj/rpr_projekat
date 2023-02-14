28e1eca7189ede7316976c03bdf12726eb377895
selmaalj <sljuhar1@etf.unsa.ba>
2023-02-14 18:14:59 +0100

    implementirane dvije akcije dugmeta

--- src/main/java/ba/unsa/etf/rpr/controllers/AdminPanel.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/AdminPanel.java
@@ -1,17 +1,41 @@
 package ba.unsa.etf.rpr.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class AdminPanel {
-    public void akcijaDugmetaDodaj(ActionEvent actionEvent) {
+
+    public void akcijaDugmetaNazad(ActionEvent actionEvent) throws IOException {
+        Node node=(Node) actionEvent.getSource();
+        Stage stage= (Stage) node.getScene().getWindow();
+        FXMLLoader fxmlLoader=new FXMLLoader(getClass().getResource("/fxml/adminprijava.fxml"));
+        Parent root=fxmlLoader.load();
+        Scene scene=new Scene(root,400,200);
+        stage.setTitle("Admin prijava");
+        stage.setScene(scene);
+        stage.show();
     }
 
-    public void akcijaDugmetaNazad(ActionEvent actionEvent) {
+    public void akcijaDugmetaPocetak(ActionEvent actionEvent) throws IOException {
+        Node node=(Node) actionEvent.getSource();
+        Stage stage= (Stage) node.getScene().getWindow();
+        FXMLLoader fxmlLoader=new FXMLLoader(getClass().getResource("/fxml/odabirwindow.fxml"));
+        Parent root=fxmlLoader.load();
+        Scene scene=new Scene(root,400,200);
+        stage.setTitle("Dobro došli!");
+        stage.setScene(scene);
+        stage.show();
     }
 
-    public void akcijaDugmetaObrisi(ActionEvent actionEvent) {
+    public void akcijaDugmetaDodajPredmet(ActionEvent actionEvent) {
     }
 
-    public void akcijaDugmetaPocetak(ActionEvent actionEvent) {
+    public void akcijaDugmetaObrisiPredmet(ActionEvent actionEvent) {
     }
 }
