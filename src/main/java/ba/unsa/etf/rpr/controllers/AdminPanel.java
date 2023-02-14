00e5f9517d437a87f86596105afe16a2f47545c2
selmaalj <sljuhar1@etf.unsa.ba>
2023-02-14 17:53:57 +0100

    kreiran tab predmet

--- src/main/java/ba/unsa/etf/rpr/controllers/AdminPanel.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/AdminPanel.java
@@ -1,4 +1,17 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.event.ActionEvent;
+
 public class AdminPanel {
+    public void akcijaDugmetaDodaj(ActionEvent actionEvent) {
+    }
+
+    public void akcijaDugmetaNazad(ActionEvent actionEvent) {
+    }
+
+    public void akcijaDugmetaObrisi(ActionEvent actionEvent) {
+    }
+
+    public void akcijaDugmetaPocetak(ActionEvent actionEvent) {
+    }
 }
