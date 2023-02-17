4760a7068735783bca21c6fcd4f80963bac9ba9a
selmaalj <sljuhar1@etf.unsa.ba>
2023-02-17 23:43:44 +0100

    dodan button

--- src/main/java/ba/unsa/etf/rpr/controllers/Korisnik.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/Korisnik.java
@@ -81,4 +81,15 @@ public class Korisnik {
             stage.show();
         }
     }
+
+    public void akcijaDugmetaNazad(ActionEvent actionEvent) throws IOException {
+        Node node= (Node) actionEvent.getSource();
+        Stage stage= (Stage) node.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/odabirwindow.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root, 400, 200);
+        stage.setTitle("Dobro došli!");
+        stage.setScene(scene);
+        stage.show();
+    }
 }
