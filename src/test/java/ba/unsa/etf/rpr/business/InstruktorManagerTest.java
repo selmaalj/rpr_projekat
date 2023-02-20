9f5915ec663bc3461665a83c27c8a3c27ce8759d
selmaalj <sljuhar1@etf.unsa.ba>
2023-02-20 22:25:08 +0100

    dodana metoda validateNaziv

--- src/test/java/ba/unsa/etf/rpr/business/InstruktorManagerTest.java
+++ src/test/java/ba/unsa/etf/rpr/business/InstruktorManagerTest.java
@@ -1,12 +1,17 @@
 package ba.unsa.etf.rpr.business;
 
+import ba.unsa.etf.rpr.Izuzetak;
 import ba.unsa.etf.rpr.dao.InstruktorDaoSQLImpl;
 import ba.unsa.etf.rpr.domain.Instruktor;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class InstruktorManagerTest {
     private InstruktorManager instruktorManager;
@@ -22,5 +27,23 @@ public class InstruktorManagerTest {
         instruktori=new ArrayList<>();
         instruktori.addAll(Arrays.asList(instruktor, new Instruktor(0, "Naziv2", "2", 2., "Grad2"),
                 new Instruktor(0, "Naziv3", "3", 3., "Grad3")));
+    }
+    @Test
+    public void validateNaziv(){
+        String correctnaziv="Ime prezime";
+        try{
+            Mockito.doCallRealMethod().when(instruktorManager).validateNaziv(correctnaziv);
+        }catch(Izuzetak e){
+            Assertions.fail();
+        }
+        String incorrectShort="I P";
+        Mockito.doCallRealMethod().when(instruktorManager).validateNaziv(incorrectShort);
+        Izuzetak instruktorException1=assertThrows(Izuzetak.class, ()-> instruktorManager.validateNaziv(incorrectShort), "Naziv mora imati i ime i prezime najmanje duzine 2!") ;
+        Assertions.assertEquals("Naziv mora imati i ime i prezime najmanje duzine 2!",instruktorException1.getMessage());
+
+        String incorrectEmpty="";
+        Mockito.doCallRealMethod().when(instruktorManager).validateNaziv(incorrectEmpty);
+        Izuzetak instruktorException2=assertThrows(Izuzetak.class,()-> instruktorManager.validateNaziv(incorrectEmpty),"Naziv mora imati i ime i prezime najmanje duzine 2!") ;
+        Assertions.assertEquals("Naziv mora imati i ime i prezime najmanje duzine 2!",instruktorException2.getMessage());
     }
 }
