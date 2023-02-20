ca16da55fe4cf5843bae6402ed61f16e2cf0ae89
selmaalj <sljuhar1@etf.unsa.ba>
2023-02-20 22:20:46 +0100

    PredmetManagerTest dodana metoda validate

--- src/test/java/ba/unsa/etf/rpr/business/PredmetManagerTest.java
+++ src/test/java/ba/unsa/etf/rpr/business/PredmetManagerTest.java
@@ -1,13 +1,18 @@
 package ba.unsa.etf.rpr.business;
 
+import ba.unsa.etf.rpr.Izuzetak;
 import ba.unsa.etf.rpr.dao.PredmetDaoSQLImpl;
 import ba.unsa.etf.rpr.domain.Predmet;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class PredmetManagerTest {
     private PredmetManager predmetManager;
@@ -24,5 +29,22 @@ public class PredmetManagerTest {
         predmeti.addAll(Arrays.asList(predmet, new Predmet(0, "Naziv2", "Nivo2"),
                 new Predmet(0, "Naziv3", "Nivo3")));
     }
+    @Test
+    public void validateNaziv(){
+        String correctnaziv="Predmet";
+        try{
+            Mockito.doCallRealMethod().when(predmetManager).validateNaziv(correctnaziv);
+        }catch(Izuzetak e){
+            Assertions.fail();
+        }
+        String incorrectShort="1";
+        Mockito.doCallRealMethod().when(predmetManager).validateNaziv(incorrectShort);
+        Izuzetak predmetException1=assertThrows(Izuzetak.class, ()-> predmetManager.validateNaziv(incorrectShort), "Naziv mora imati i ime i prezime najmanje duzine 2!") ;
+        Assertions.assertEquals("Naziv mora biti duzine 2 minimum!",predmetException1.getMessage());
 
+        String incorrectEmpty="";
+        Mockito.doCallRealMethod().when(predmetManager).validateNaziv(incorrectEmpty);
+        Izuzetak predmetException2=assertThrows(Izuzetak.class,()-> predmetManager.validateNaziv(incorrectEmpty),"Naziv mora imati i ime i prezime najmanje duzine 2!") ;
+        Assertions.assertEquals("Naziv mora biti duzine 2 minimum!",predmetException2.getMessage());
+    }
 }
